95bd3182f72582fcd153be30421bd33e5d8aa2d9
Abdullah-Arfan <abdullahateebatik@gmail.com>
2025-08-16 19:57:46 +0500

    global.css and layout

--- myapp/src/app/layout.jsx
+++ myapp/src/app/layout.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import "../style/global.css"
-// import "./../Style/Global2.css"
-import Navbar from "../Components/Navbar"
-import Footer from "../Components/Footer"
+import React from "react";
+import "../style/global.css"; // Make sure folder/file names match
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
-export default function Rootlayout({ children }) {
-    return (
-        <html lang="en">
-            <body>
-                <Navbar/>
-                <main>{children}</main>
-                <Footer/>
-            </body>
-        </html>
-    )
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
 }
+
+
+
+// import React from 'react'
+// import "../style/global.css"
+// // import "./../Style/Global2.css"
+// import Navbar from "../Components/Navbar"
+// import Footer from "../Components/Footer"
+
+// export default function Rootlayout({ children }) {
+//     return (
+//         <html lang="en">
+//             <body>
+//                 <Navbar/>
+//                 <main>{children}</main>
+//                 <Footer/>
+//             </body>
+//         </html>
+//     )
+// }
